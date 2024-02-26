aa9d889fa49768d33c3ea0752f5d6c21d3ed052c
유영 <dbdud1999@naver.com>
2024-02-27 00:13:43 +0900

    :sparkles: 테스트용 Business VO, DAO 연결

---
+++ Business/src/main/java/com/prp/business/model/vo/BusinessVo.java
@@ -0,0 +1,11 @@
+package com.prp.business.model.vo;
+
+import java.sql.Timestamp;
+
+public class BusinessVo {
+    private String writerId;
+    private String title;
+    private String content;
+    private Timestamp createdDate;
+    private Timestamp updatedDate;
+}
