7affd0ad83c4c648d025f8915fc77e9afc6e5879
유영 <dbdud1999@naver.com>
2024-02-27 23:31:58 +0900

    :white_check_mark: 업무 리스트 조회 서비스 테스트

--- Business/src/test/java/com/prp/business/model/service/BusinessServiceTest.java
+++ Business/src/test/java/com/prp/business/model/service/BusinessServiceTest.java
@@ -1,12 +1,15 @@
 package com.prp.business.model.service;
 
-import com.prp.business.model.dao.BusinessDao;
 import com.prp.business.model.dto.BusinessCreateCommandDto;
 import com.prp.business.model.dto.BusinessListCreateCommandDto;
+import com.prp.business.model.dto.BusinessListReadResponseDto;
+import com.prp.business.util.BusinessException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +24,9 @@ public class BusinessServiceTest {
     }
 
     @Test
-    public void 업무_생성() {
+    @DisplayName("업무 생성")
+    @Transactional
+    public void setBusinessList() {
         // given
         List<BusinessCreateCommandDto> businessList = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
@@ -37,46 +42,22 @@ public class BusinessServiceTest {
                 .build();
 
         // when
-        boolean flag = businessService.createBusiness(businessListCreateCommandDto);
+        boolean flag = businessService.setBusinessList(businessListCreateCommandDto);
 
         // then
         Assertions.assertTrue(flag, "저장 실패!");
     }
     @Test
-    public void 업무_생성_업무가_비었을_때() throws RuntimeException {
+    @DisplayName("업무 조회")
+    @Transactional
+    public void getBusinessList() {
         // given
-        List<BusinessCreateCommandDto> businessList = new ArrayList<>();
-        BusinessListCreateCommandDto businessListCreateCommandDto = BusinessListCreateCommandDto.builder()
-                .writer("작성자 Test")
-                .businessList(businessList)
-                .build();
 
         // when
-        boolean flag = businessService.createBusiness(businessListCreateCommandDto);
+        List<BusinessListReadResponseDto> businessDtoList = businessService.getBusinessList();
 
         // then
-        Assertions.assertTrue(flag, "저장 실패!");
-    }
-    @Test
-    public void 업무_생성_작성자가_비었을_때() throws RuntimeException {
-        // given
-        List<BusinessCreateCommandDto> businessList = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            businessList.add(BusinessCreateCommandDto.builder()
-                    .title("제목 Test")
-                    .content("내용 Test... 내용 Test... 내용 Test...")
-                    .build()
-            );
-        }
-        BusinessListCreateCommandDto businessListCreateCommandDto = BusinessListCreateCommandDto.builder()
-                .writer(null)
-                .businessList(businessList)
-                .build();
-
-        // when
-        boolean flag = businessService.createBusiness(businessListCreateCommandDto);
-
-        // then
-        Assertions.assertTrue(flag, "저장 실패!");
+        Assertions.assertNotEquals(null, businessDtoList);
+        Assertions.assertNotEquals(0, businessDtoList.size());
     }
 }
